6169e43ae009b90c0b8806da0cf999820ece6fc6
ekshant <74665871+ekshant@users.noreply.github.com>
2020-12-06 15:19:13 +0000

    Updated Attempt 6
    The probabilities weren't being read correctly in the previous code, hence the code was still reaching 0 and we were getting an error when it reached lane 10. I added a 'strip' function within the function1 code so the brackets and commas can be deleted from the probabilities list and added a for loop so the correct probabilities can be added to the list as a float. This allowed the 'weights' part to read and apply the correct probabilities. 
    
    Also added a for loop to simulate the generation of steps.
    
    Also added a frequency count for each lane within a for loop with increasing increments of 10. But this only does it for each separate simulation, not the total of the entire simulation.

--- Attempt 6.py
+++ Attempt 6.py
@@ -1,71 +1,92 @@
 import csv
 import random
 
-
 #I've changed function1 so it now takes in 2 inputs (x and y co-ordinates
 #of the probability table) and will output the correct probability 
 def function1(x,y):
     last_turns=[]
     lanes=[]
-    prob = open('probabilities.csv')
+    prob = open('Probabilities.csv')
     readprob = csv.reader(prob)
     for column in readprob:
         last_turns.append(column[x])#appends entire given column
-    prob.close()
     #print(last_turns)
-    lane_num=last_turns[y]
-    print(lane_num) #prints correct row from given column
-
-
-########################
-#I still apply random.choices function which considers the weighted
-#probabilities
+    lane_prob=last_turns[y]
+    prob.close()
+    words=lane_prob.strip("()")
+    words2=words.split(",")
+    a=[]
+    for i in words2:
+        a.append(float(i))
+    return a #prints correct row from given column
     
 
 direction = ['L','F','R']
+for k in range(5):
+    
+    #Already know initial step so I simulate the second step
+    steps = ['F']
+    lane = [5]
+    prob2 = (0.30,0.40,0.30)
+    step=random.choices(direction,weights=prob2,k=1)
+    steps.append(step[0])
 
-#Already know initial step so I simulate the second step, given the
-#first step
+    #print("\nfor loop")          
+    for i in range(100):
+        #print("\n")
+        #determine the x value from the table by looking at the direction
+        #of the previous 2 steps
+        if (steps[-1]=='L') and (steps[-2]!='L'):
+            x=1
+        elif (steps[-1]=='L') and (steps[-2]=='L'):
+            x=2
+        elif (steps[-1]=='F') and (steps[-2]!='F'):
+            x=3
+        elif (steps[-1]=='F') and (steps[-2]=='F'):
+            x=4
+        elif (steps[-1]=='R') and (steps[-2]!='R'):
+            x=5
+        else:
+            x=6
+        #add to the list 'lane' to store which lane each step is taken in
+        if steps[-1]=='L': #changed step to steps
+            next_lane=lane[-1]-1
+        elif steps[-1]=='F':
+            next_lane=lane[-1]
+        else:
+            next_lane=lane[-1]+1
+        lane.append(next_lane)
+        #determine the y value by looking at what lane the last step was in
+        y=lane[-1]
+        #function1(x,y)
+        next_prob=function1(x,y)#call the relevent probability
+        #print(next_prob)
+        #print('Lane: '+str(y))
+        step=random.choices(direction,weights=next_prob,k=1)
+        steps.append(step[0])#add this step to the steps list
+        #print(steps)
+        #print(lane)
 
-steps = ['F']#starting initially with an F
-lane = [5]#starting initially in lane 5
+for i in range(10,101,10):
+    lane_count=lane[0:i]
+    print('\nFrequencies for the first '+str(i)+' steps')
+    lane1_freq=lane_count.count(1)
+    print('Lane 1: ' + str(lane1_freq))
+    lane2_freq=lane_count.count(2)
+    print('Lane 2: ' + str(lane2_freq))
+    lane3_freq=lane_count.count(3)
+    print('Lane 3: ' + str(lane3_freq))
+    lane4_freq=lane_count.count(4)
+    print('Lane 4: ' + str(lane4_freq))
+    lane5_freq=lane_count.count(5)
+    print('Lane 5: ' + str(lane5_freq))
+    lane6_freq=lane_count.count(6)
+    print('Lane 6: ' + str(lane6_freq))
+    lane7_freq=lane_count.count(7)
+    print('Lane 7: ' + str(lane7_freq))
+    lane8_freq=lane_count.count(8)
+    print('Lane 8: ' + str(lane8_freq))
+    lane9_freq=lane_count.count(9)
+    print('Lane 9: ' + str(lane9_freq))
+        
 
-#testing for the first 10 steps only
-print("\nfor loop")          
-for i in range(10):
-    #determine the x value from the table by looking at the direction
-    #of the previous 2 steps
-    if (len(steps)==1): #the second step will only have 1 previous step
-                        #stored
-        x=5
-    if (steps[-1]=='L') and (steps[-2]!='L'):
-        x=1
-    elif (steps[-1]=='L') and (steps[-2]=='L'):
-        x=2
-    elif (steps[-1]=='F') and (steps[-2]!='F'):
-        x=3
-    elif (steps[-1]=='F') and (steps[-2]=='F'):
-        x=4
-    elif (steps[-1]=='R') and (steps[-2]!='R'):
-        x=5
-    else:
-        x=6
-    #add to the list 'lane' to store which lane each step is taken in
-    if steps[-1]=='L':
-        next_lane=lane[-1]-1
-    elif steps[-1]=='F':
-        next_lane=lane[-1]
-    else:
-        next_lane=lane[-1]+1
-    lane.append(next_lane)
-    #determine the y value by looking at what lane the last step was in
-    y=lane[-1]
-    next_prob=function1(x,y)#call the relevent probability
-    #print(next_prob)
-    print(y)
-    step=random.choices(direction,weights=next_prob)#generate the direction of the
-                                            #next step
-    steps.append(step[0])#add this step to the steps list
-    print(steps)
-    #print(lane)
- 
